05b5936bbc538005c265f34ab0bf296611274c27
Stefan Steiner <ssteiner@berkeley.edu>
2017-01-02 17:36:30 -0500

    gon not working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
 	def index
+		# parse data for all addresses as strings
 		@addresses = []
 		@count = 0
 		file = File.read('app/assets/monuments.json')
@@ -7,9 +8,20 @@ class WelcomeController < ApplicationController
 		@monuments["data"].each do |monument|
 			@count += 1
 			@addresses << monument[10]
+		end
 		gon.count = @count
 		gon.addresses = @addresses
+
+		# find all monument pairs
+		@pairs = []
+		(0..@count-2).each do |i|
+			(i+1..@count-1).each do |j|
+				@pairs << {origin:@addresses[i], destination:@addresses[j], panelName:"p"+j.to_s}
+			end
 		end
+		gon.pairs = @pairs
+
+		@first = @addresses[0]
 	end
 	
 	def show
