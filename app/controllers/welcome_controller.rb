4133d0fccaa647e0c3aaf7f18934de67648e0401
Stefan Steiner <ssteiner@berkeley.edu>
2017-01-02 14:30:15 -0500

    list of addresses

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,11 @@
 class WelcomeController < ApplicationController
 	def index
+		@addresses = []
+		file = File.read('app/assets/monuments.json')
+		@monuments = JSON.parse(file)
+		@monuments["data"].each do |monument|
+			@addresses << monument[10]
+		end
 	end
 	
 	def show
